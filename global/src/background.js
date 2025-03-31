7797fd47862b63e32f48bf5f1575f9570aab51cc
juliancruzsanchez <20978820+juliancruzsanchez@users.noreply.github.com>
2025-03-31 12:19:38 -0600

    Added setSetting for context.js

--- global/src/background.js
+++ global/src/background.js
@@ -1,11 +1,27 @@
-  // background.js
-  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "getSetting") {
-      chrome.storage.sync.get(request.setting, (result) => {
-        sendResponse({ value: result[request.setting] });
-      });
-      return true;  // Indicate that sendResponse will be called asynchronously
-    }
-  });
+// background.js
 
-  
+chrome.runtime.onStartup.addListener(() => {
+  console.log(`onStartup()`);
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "getSetting") {
+    console.log("getSetting:", request.setting)
+    chrome.storage.sync.get(request.setting, (result) => {
+      sendResponse({ value: result[request.setting] });
+    });
+    return true;  // Indicate that sendResponse will be called asynchronously
+  } else if (request.action === 'setSetting') {
+    setSetting(request.setting, request.value);
+    sendResponse({ value: 'ok' });
+    return true;
+  }
+});
+
+function setSetting(setting, val) {
+  let set = {};
+  set[setting] = val;
+  chrome.storage.sync.set(set).then(() => {
+    console.log(`Set ${setting} to ${val}`);
+  });
+} 
